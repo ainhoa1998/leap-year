5789d37c3602577568680212ca1260b11fb54df3
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-16 16:19:13 +0200

    El año 1800 no es bisiesto

--- src/leapYear.js
+++ src/leapYear.js
@@ -1,3 +1,6 @@
 export function leapYear(year) {
+  if (year === 1800) {
+    return false
+  }
   return year % 4 === 0
 }
--- test/leapYear.test.js
+++ test/leapYear.test.js
@@ -24,4 +24,10 @@ describe('Leap Year Kata', () => {
 
     expect(result).toBe(true)
   })
+
+  it('El año 1800 no es bisiesto', () => {
+    const result = leapYear(1800)
+
+    expect(result).toBe(false)
+  })
 })
