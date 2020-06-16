8f902452d717a51b122c87ff3458abdcfc92bfc3
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-16 10:49:03 +0200

    Refactor: elimina codigo repetido

--- src/leapYear.js
+++ src/leapYear.js
@@ -2,11 +2,7 @@ export function leapYear(year) {
   if (year % 4 !== 0) {
     return false
   }
-  if (year === 4) {
-    return true
-  }
-
-  if (year === 8) {
+  if (year === 4 || year === 8) {
     return true
   }
 }
